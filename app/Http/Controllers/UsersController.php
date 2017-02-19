5f0fa9427388a0b250125dd36c2d7a924a15e558
Greg <gperlman27@gmail.com>
2017-02-19 10:37:04 -0500

    return token upon user creation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 use App\User;
 
@@ -10,11 +12,27 @@ class UsersController extends Controller
 {
   protected function create(Request $request)
   {
-      return User::create([
+      User::create([
           'name' => $request->name,
           'email' => $request->email,
           'password' => bcrypt($request->password),
           'currentWeek' => $request->currentWeek,
       ]);
+
+      // grab credentials from the request
+      $credentials = $request->only('email', 'password');
+
+      try {
+          // attempt to verify the credentials and create a token for the user
+          if (! $token = JWTAuth::attempt($credentials)) {
+              return response()->json(['error' => 'invalid_credentials'], 401);
+          }
+      } catch (JWTException $e) {
+          // something went wrong whilst attempting to encode the token
+          return response()->json(['error' => 'could_not_create_token'], 500);
+      }
+
+      // all good so return the token
+      return response()->json(compact('token'));
   }
 }
